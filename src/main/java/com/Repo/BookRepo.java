0ec59ba86c735eb6becbc7581b0af4664328ffe9
syahril20 <rodyulo20@gmail.com>
2023-02-26 21:59:13 +0700

    HAP

--- src/main/java/com/Repo/BookRepo.java
+++ src/main/java/com/Repo/BookRepo.java
@@ -1,9 +1,9 @@
 package com.Repo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Model.Book;
 
-public interface BookRepo extends CrudRepository<Book, Long>{
+public interface BookRepo extends JpaRepository<Book, Long>{
     
 }
